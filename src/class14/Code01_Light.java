a9839e6467de8f8a9c58e96cd8e0240f66747e17
fuyongchun <f578521721@163.com>
2022-01-12 23:58:29 +0800

    day15 练习加注释

--- src/class14/Code01_Light.java
+++ src/class14/Code01_Light.java
@@ -2,85 +2,114 @@ package class14;
 
 import java.util.HashSet;
 
-//
+//字符串,X是墙,.是可以挂灯的地方,灯可以照亮左右和自己,墙照不照无所谓,给你一个字符串,问你灯最少多少
 public class Code01_Light {
 
-	public static int minLight1(String road) {
-		if (road == null || road.length() == 0) {
-			return 0;
-		}
-		return process(road.toCharArray(), 0, new HashSet<>());
-	}
+    public static int minLight1(String road) {
+        if (road == null || road.length() == 0) {
+            return 0;
+        }
+        return process(road.toCharArray(), 0, new HashSet<>());
+    }
 
-	// str[index....]位置，自由选择放灯还是不放灯
-	// str[0..index-1]位置呢？已经做完决定了，那些放了灯的位置，存在lights里
-	// 要求选出能照亮所有.的方案，并且在这些有效的方案中，返回最少需要几个灯
-	public static int process(char[] str, int index, HashSet<Integer> lights) {
-		if (index == str.length) { // 结束的时候
-			for (int i = 0; i < str.length; i++) {
-				if (str[i] != 'X') { // 当前位置是点的话
-					if (!lights.contains(i - 1) && !lights.contains(i) && !lights.contains(i + 1)) {
-						return Integer.MAX_VALUE;
-					}
-				}
-			}
-			return lights.size();
-		} else { // str还没结束
-			// i X .
-			int no = process(str, index + 1, lights);
-			int yes = Integer.MAX_VALUE;
-			if (str[index] == '.') {
-				lights.add(index);
-				yes = process(str, index + 1, lights);
-				lights.remove(index);
-			}
-			return Math.min(no, yes);
-		}
-	}
+    // str[index....]位置，自由选择放灯还是不放灯
+    // str[0..index-1]位置呢？已经做完决定了，那些放了灯的位置，存在lights里
+    // 要求选出能照亮所有.的方案，并且在这些有效的方案中，返回最少需要几个灯
+    public static int process(char[] str, int index, HashSet<Integer> lights) {
+        if (index == str.length) { // 结束的时候
+            for (int i = 0; i < str.length; i++) {
+                if (str[i] != 'X') { // 当前位置是点的话
+                    if (!lights.contains(i - 1) && !lights.contains(i) && !lights.contains(i + 1)) {
+                        return Integer.MAX_VALUE;
+                    }
+                }
+            }
+            return lights.size();
+        } else { // str还没结束
+            // i X .
+            int no = process(str, index + 1, lights);
+            int yes = Integer.MAX_VALUE;
+            if (str[index] == '.') {
+                lights.add(index);
+                yes = process(str, index + 1, lights);
+                lights.remove(index);
+            }
+            return Math.min(no, yes);
+        }
+    }
 
-	public static int minLight2(String road) {
-		char[] str = road.toCharArray();
-		int i = 0;
-		int light = 0;
-		while (i < str.length) {
-			if (str[i] == 'X') {
-				i++;
-			} else {
-				light++;
-				if (i + 1 == str.length) {
-					break;
-				} else { // 有i位置  i+ 1   X  .
-					if (str[i + 1] == 'X') {
-						i = i + 2;
-					} else {
-						i = i + 3;
-					}
-				}
-			}
-		}
-		return light;
-	}
+    public static int minLight2(String road) {
+        char[] str = road.toCharArray();
+        int i = 0;
+        int light = 0;
+        while (i < str.length) {
+            //i位置是墙,下一轮
+            if (str[i] == 'X') {
+                i++;
+            } else {
+                //灯先+1(总结了规律后得出的,只要i不是墙,总会+1的)
+                light++;
+                //i+1是最后了,不管了
+                if (i + 1 == str.length) {
+                    break;
+                } else { // 有i位置  i+ 1   X  .
+                    if (str[i + 1] == 'X') {
+                        //放在i,i+1是X,跑到i+2继续执行
+                        i = i + 2;
+                    } else {
+                        //
+                        i = i + 3;
+                    }
+                }
+            }
+        }
+        return light;
+    }
 
-	// for test
-	public static String randomString(int len) {
-		char[] res = new char[(int) (Math.random() * len) + 1];
-		for (int i = 0; i < res.length; i++) {
-			res[i] = Math.random() < 0.5 ? 'X' : '.';
-		}
-		return String.valueOf(res);
-	}
+    // for test
+    public static String randomString(int len) {
+        char[] res = new char[(int) (Math.random() * len) + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = Math.random() < 0.5 ? 'X' : '.';
+        }
+        return String.valueOf(res);
+    }
 
-	public static void main(String[] args) {
-		int len = 20;
-		int testTime = 100000;
-		for (int i = 0; i < testTime; i++) {
-			String test = randomString(len);
-			int ans1 = minLight1(test);
-			int ans2 = minLight2(test);
-			if (ans1 != ans2) {
-				System.out.println("oops!");
-			}
-		}
-		System.out.println("finish!");
-	}
+    public static void main(String[] args) {
+        int len = 20;
+        int testTime = 100000;
+        for (int i = 0; i < testTime; i++) {
+            String test = randomString(len);
+            int ans1 = minLight1(test);
+            int ans2 = minLight2(test);
+            if (ans1 != ans2) {
+                System.out.println("oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+
+    //练习
+    public static int minLight3(String road) {
+        char[] str = road.toCharArray();
+        int i = 0;
+        int light = 0;
+        while (i < str.length) {
+            if (str[i] == 'X') {
+                break;
+            } else {
+                light++;
+                if (i + 1 == str.length) {
+                    break;
+                } else {
+                    if (str[i + 1] == 'X') {
+                        i = i + 2;
+                    } else {
+                        i = i + 3;
+                    }
+                }
+            }
+        }
+        return light;
+    }
 }
--- src/class14/Code05_UnionFind.java
+++ src/class14/Code05_UnionFind.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 
+//并查集  一个大集合,里面有一堆节点,要求有两个函数,一个是isSameSet,是不是同一个集合的,Union合并集合
+//要求这两个函数时间复杂度常数
+//做法,每个节点建一个指针指向自己,当合并的时候,检查集合大小,小的找到找到自己第一个爹,指到大的爹下面
+//nodes,以v值为头的集合,parents,每个节点有唯一的爹,sizemap,根据头知道自己集合大小,快速对比,进行合并,然后被合并的那个删掉记录
 public class Code05_UnionFind {
 
 	public static class Node<V> {
@@ -32,12 +36,15 @@ public class Code05_UnionFind {
 		}
 
 		// 给你一个节点，请你往上到不能再往上，把代表返回
+		//就是爹不是自己,继续,爹是自己,自己就是大爹根节点
 		public Node<V> findFather(Node<V> cur) {
 			Stack<Node<V>> path = new Stack<>();
+			//优化点,合并第一次,用栈接受沿途节点,并将根节点拿到
 			while (cur != parents.get(cur)) {
 				path.push(cur);
 				cur = parents.get(cur);
 			}
+			//之后将路径上所有节点父节点都设置为头节点,下次使用可以节约查找到头节点需要的时间
 			while (!path.isEmpty()) {
 				parents.put(path.pop(), cur);
 			}
