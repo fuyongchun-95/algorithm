5904e927b6a8299b38734bc58f32bb9ada2620f5
fuyongchun <f578521721@163.com>
2022-01-17 19:31:11 +0800

    day17 注释+练习

--- src/class16/Code01_BFS.java
+++ src/class16/Code01_BFS.java
@@ -12,6 +12,7 @@ public class Code01_BFS {
 			return;
 		}
 		Queue<Node> queue = new LinkedList<>();
+		//set集合每次入栈校验,存在就不入栈,因为存在还入栈就会导致结果变多
 		HashSet<Node> set = new HashSet<>();
 		queue.add(start);
 		set.add(start);
@@ -27,4 +28,24 @@ public class Code01_BFS {
 		}
 	}
 
+	//练习  根树的层序遍历很像
+	public static void bfs1(Node start) {
+		if (start==null){
+			return;
+		}
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(start);
+		HashSet<Node> set = new HashSet<>();
+		set.add(start);
+		while (!queue.isEmpty()){
+			Node cur = queue.poll();
+			System.out.println(cur.value);
+			for (Node next : cur.nexts) {
+				if (!set.contains(next)){
+					set.add(next);
+					queue.add(next);
+				}
+			}
+		}
+	}
 }
